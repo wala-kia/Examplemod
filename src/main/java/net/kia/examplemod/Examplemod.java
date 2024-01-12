6efb7b5f82232724a1a8176076bbd18ed0855498
kia <wala.kia08@gmail.com>
2024-01-12 16:27:51 -0700

    Initial commit

---
+++ src/main/java/net/kia/examplemod/Examplemod.java
@@ -0,0 +1,16 @@
+package net.kia.examplemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Examplemod implements ModInitializer {
+	public static final String MOD_ID = "examplemod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/kia/examplemod/Examplemodclient.java
@@ -0,0 +1,10 @@
+package net.kia.examplemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Examplemodclient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
